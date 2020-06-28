cd52b148288be90b6f893b545cbee61d742392d7
yanghaotong <yanghaotong55@126.com>
2020-06-28 17:48:45 +0800

    修改文件名大小写

--- src/com/crAdmin/module/stock/controller/StockController.java
+++ src/com/crAdmin/module/stock/controller/StockController.java
@@ -476,7 +476,7 @@ public class StockController extends BaseController {
 		model.addAttribute("startTime", startTime);
 		model.addAttribute("endTime", endTime);
 		model.addAttribute("dateType", dateType == null ? "" : dateType);
-		return "statistics/stock/outStock.ftl";
+		return "statistics/stock/outStock3.ftl";
 	}
 
 	/**
